2e51a70be2737543e4179e0d60f238f72ea681fe
alamindesign <mail.muhammadit@gmail.com>
2022-12-15 21:22:02 +0600

    first time js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 this is for multi-line 
 comment.
 */
+var btn = document.getElementById("go-button");
+function buttonClicked(){
+    console.log("button clicked.");
+    btn.removeEventListener("click",buttonClicked);
+}
+btn.addEventListener("click",buttonClicked);
