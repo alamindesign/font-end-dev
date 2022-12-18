4a62145334f39ce1780b7430d8d66d4d5be7e620
alamindesign <mail.muhammadit@gmail.com>
2022-12-18 18:49:43 +0600

    learning js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,30 @@
 this is for multi-line 
 comment.
 */
-var btn = document.getElementById("go-button");
-function buttonClicked(){
-    console.log("button clicked.");
-    btn.removeEventListener("click",buttonClicked);
+let btn = document.getElementById("go-button");
+function clickedOnButton(){
+    console.log("Clicked on button");
+    btn.removeEventListener("click",clickedOnButton);
+    document.getElementById("text").innerHTML="<b>Don't</b> do it!!"
 }
-btn.addEventListener("click",buttonClicked);
+btn.addEventListener("click",clickedOnButton);
+//replace content after clicking the button...
+let showImg = document.getElementById("show-me");
+function showImage(){
+    showImg.removeEventListener("click",showImage);
+    document.getElementById("img").innerHTML = "I don't know how to show image form javascript, sorry!!"
+    
+}
+showImg.addEventListener("click",showImage);
+let hobbies = ["pizza","Gamming","Readding","Programming","music"];
+console.log(hobbies.pop());
+hobbies.push("Archari");
+//loop through array
+hobbies.forEach(function(item){
+    console.log("I like",item);
+});
+let indexNum = hobbies.indexOf("Gamming");
+hobbies.splice(indexNum,1);
+hobbies.forEach(function(i,e){
+    console.log("I like",i,e);
+})
