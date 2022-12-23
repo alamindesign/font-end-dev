81270b13a794083f4265684582a0ab4338f5e2e0
alamindesign <mail.muhammadit@gmail.com>
2022-12-23 13:16:02 +0600

    lot about class

--- js/extended-object.js
+++ js/extended-object.js
@@ -10,10 +10,43 @@ class miniAnimal {
     }
 }
 
-miniAnimal.planet = "Moon";
+class MiniDog extends miniAnimal {
+    constructor(name,height,weight,barkVoliume,leashColor,speed){
+        super(name,height,weight);
+        this.barkVoliume = barkVoliume;
+        this.leashColor = leashColor;
+        this.speed = speed;
+    }
 
-let miniDog = new miniAnimal("Tommy");
-let miniFish = new miniAnimal("Fishhy");
+    bark(){
+        if(this.barkVoliume > 50){
+            console.log(this.name, "barks loudly! (voliume:",this.barkVoliume, ")");
+        }else{
+            console.log(this.name, "burks timitly (voliume:",this.barkVoliume, ")");
+        }
+    }
+}
 
-console.log(miniDog.name, miniDog.nameLen());
-console.log(miniDog.constructor.planet);
+class MiniFish extends miniAnimal{
+    constructor(name,height,weight,swimSpeed){
+        super(name,height,weight);
+        this.swimSpeed = swimSpeed;
+    }
+    swim(dogspeed){
+        if(this.swimSpeed > 50){
+            console.log(this.name, "Swims very fast (speed:",this.swimSpeed,")");
+        }else{
+            console.log(this.name,"Swims very Slow (speed:",this.swimSpeed,")");
+        }
+        if(dogspeed > this.swimSpeed){
+            console.log(this.name,"Got cought.");
+        }else{
+            console.log(this.name,"Get away.");
+        }
+    }
+} 
+
+let king = new MiniDog("King",45,92,70,"Golden",62);
+let Goldee = new MiniFish("Goldee",10,.20,60);
+king.bark()
+Goldee.swim(king.speed)
